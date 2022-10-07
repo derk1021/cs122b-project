deaee533e3aa9070808b251e4d50b0e047980a10
Kei Asakawa <77090883+Kaiyooon@users.noreply.github.com>
2022-10-07 16:11:57 -0700

    Update

---
+++ src/main/webapp/index.js
@@ -0,0 +1,43 @@
+function handleStarResult(resultData) {
+    console.log("handleStarResult: populating star table from resultData");
+
+    // Populate the star table
+    // Find the empty table body by id "star_table_body"
+    let starTableBodyElement = jQuery("#star_table_body");
+    let rowHTML = "<tr>";
+    rowHTML = "<th>Hello</th>";
+    rowHTML = "<th>World</th>";
+    rowHTML += "</tr>";
+    starTableBodyElement.append(rowHTML);
+    // Iterate through resultData, no more than 10 entries
+    for (let i = 0; i < Math.min(10, resultData.length); i++) {
+
+        // Concatenate the html tags with resultData jsonObject
+        let rowHTML = "";
+        rowHTML += "<tr>";
+        rowHTML +=
+            "<th>" +
+            // Add a link to single-star.html with id passed with GET url parameter
+            resultData[i]['movie_title']// display star_name for the link text
+            "</th>";
+        rowHTML += "<th>" + resultData[i]["movie_year"] + "</th>";
+        rowHTML += "<th>" + resultData[i]["movie_director"] + "</th>";
+        rowHTML += "</tr>";
+
+        // Append the row created to the table body, which will refresh the page
+        starTableBodyElement.append(rowHTML);
+    }
+}
+
+
+/**
+ * Once this .js is loaded, following scripts will be executed by the browser
+ */
+
+// Makes the HTTP GET request and registers on success callback function handleStarResult
+jQuery.ajax({
+    dataType: "json", // Setting return data type
+    method: "GET", // Setting request method
+    url: "api/stars", // Setting request url, which is mapped by StarsServlet in Stars.java
+    success: (resultData) => handleStarResult(resultData) // Setting callback function to handle data returned successfully by the StarsServlet
+});
