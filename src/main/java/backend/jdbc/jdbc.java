17c157409a9c8a28c97fce7b01d73e597f9f042d
Kei Asakawa <77090883+Kaiyooon@users.noreply.github.com>
2022-10-06 18:42:40 -0700

    Updates

--- src/main/java/backend/jdbc/jdbc.java
+++ src/main/java/backend/jdbc/jdbc.java
@@ -17,8 +17,24 @@ public class jdbc implements Parameters {
             Statement select = connection.createStatement();
             ResultSet result = select.executeQuery(
                     // movies
-                    "SELECT * from movies");
-
+                    "select movies.title, movies.year, movies.director, genres.name, stars.name, ratings.rating\n" +
+                            "from movies, genres, stars, ratings, stars_in_movies, genres_in_movies\n" +
+                            "where movies.id in (SELECT * FROM (select movies.id as mid\n" +
+                            "from movies, ratings\n" +
+                            "where movies.id = ratings.movieId\n" +
+                            "order by rating DESC\n" +
+                            "LIMIT 20) as t)\n" +
+                            "and movies.id = genres_in_movies.movieId and genres_in_movies.genreId = genres.id and stars_in_movies.starId = stars.id and stars_in_movies.movieId = movies.id and movies.id = ratings.movieId\n" +
+                            "ORDER BY rating desc, movies.id");
+            while (result.next()) {
+                System.out.println("Title = " + result.getString("title"));
+                System.out.println("Year = " + result.getString("year"));
+                System.out.println("Director = " + result.getString("director"));
+                System.out.println("Genre = " + result.getString("genres.name"));
+                System.out.println("Star = " + result.getString("stars.name"));
+                System.out.println("Rating = " + result.getString("rating"));
+                System.out.println();
+            }
         }
         else {
             System.out.println("no connection");
