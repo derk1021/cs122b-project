49095897124c7e08061a8a1c932bcb278f253cc5
derk1021 <derekbl@uci.edu>
2022-11-07 22:29:42 +0530

    prod url update

--- fabflix-frontend/src/environments/environment.prod.ts
+++ fabflix-frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   baseUrl:
-    'https:/ec2-15-206-238-236.ap-south-1.compute.amazonaws.com:8443/backend/api',
+    'https://ec2-15-206-238-236.ap-south-1.compute.amazonaws.com:8443/backend/api',
 };
