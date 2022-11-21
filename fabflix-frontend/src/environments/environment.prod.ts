5daad39c1a02f4ad92f10de1dcfd402820362d06
derk1021 <derekbl@uci.edu>
2022-11-22 03:46:07 +0530

    changing instance url

--- fabflix-frontend/src/environments/environment.prod.ts
+++ fabflix-frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   baseUrl:
-    'http://ec2-15-206-238-236.ap-south-1.compute.amazonaws.com:8080/backend/api',
+    'http://ec2-13-56-8-234.us-west-1.compute.amazonaws.com:8080/backend/api',
 };
